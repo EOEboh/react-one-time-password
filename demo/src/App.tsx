4969b4214c03b2c647cf9a396d2abd1d5dba1264
xosnrdev <successxodev@gmail.com>
2024-09-12 10:05:35 -0400

    chore: demo initialization with vite

---
+++ demo/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='container' style={{
+      display: 'flex',
+      flexDirection: 'column',
+      placeItems: 'center',
+    }}>Hello World</div>
+  )
+}
